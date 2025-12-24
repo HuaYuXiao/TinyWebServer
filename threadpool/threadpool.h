16a596d7723d6030784e87599cd5f45ac93285bd
Eason Hua <hyx020222@gmail.com>
2025-12-24 22:40:40 +0800

    feat: std::unique_ptr

--- threadpool/threadpool.h
+++ threadpool/threadpool.h
@@ -6,7 +6,7 @@
 #include <exception>
 #include <pthread.h>
 #include <mutex>
-#include "../lock/locker.h"
+#include <memory>
 #include "../CGImysql/sql_connection_pool.h"
 
 template <typename T>
@@ -15,7 +15,7 @@ class threadpool
 public:
     /*thread_number是线程池中线程的数量，max_requests是请求队列中最多允许的、等待处理的请求的数量*/
     threadpool(int actor_model, connection_pool *connPool, int thread_number = 8, int max_request = 10000);
-    ~threadpool();
+    ~threadpool(){};
     bool append(T *request, int state);
     bool append_p(T *request);
 
@@ -27,40 +27,34 @@ private:
 private:
     int m_thread_number;        //线程池中的线程数
     int m_max_requests;         //请求队列中允许的最大请求数
-    pthread_t *m_threads;       //描述线程池的数组，其大小为m_thread_number
+    std::unique_ptr<pthread_t[]> m_threads; //使用智能指针管理线程数组
     std::deque<T *> m_workqueue; //请求队列
     std::mutex m_queuelocker;       //保护请求队列的互斥锁
     sem m_queuestat;            //是否有任务需要处理
     connection_pool *m_connPool;  //数据库
     int m_actor_model;          //模型切换
 };
+
 template <typename T>
-threadpool<T>::threadpool( int actor_model, connection_pool *connPool, int thread_number, int max_requests) : m_actor_model(actor_model),m_thread_number(thread_number), m_max_requests(max_requests), m_threads(NULL),m_connPool(connPool)
+threadpool<T>::threadpool(int actor_model, connection_pool *connPool, int thread_number, int max_requests)
+    : m_actor_model(actor_model), m_thread_number(thread_number), m_max_requests(max_requests), m_connPool(connPool)
 {
     if (thread_number <= 0 || max_requests <= 0)
         throw std::exception();
-    m_threads = new pthread_t[m_thread_number];
-    if (!m_threads)
-        throw std::exception();
+    m_threads = std::make_unique<pthread_t[]>(m_thread_number);
     for (int i = 0; i < thread_number; ++i)
     {
-        if (pthread_create(m_threads + i, NULL, worker, this) != 0)
+        if (pthread_create(&m_threads[i], NULL, worker, this) != 0)
         {
-            delete[] m_threads;
             throw std::exception();
         }
         if (pthread_detach(m_threads[i]))
         {
-            delete[] m_threads;
             throw std::exception();
         }
     }
 }
-template <typename T>
-threadpool<T>::~threadpool()
-{
-    delete[] m_threads;
-}
+
 template <typename T>
 bool threadpool<T>::append(T *request, int state)
 {
@@ -76,6 +70,7 @@ bool threadpool<T>::append(T *request, int state)
     m_queuestat.post();
     return true;
 }
+
 template <typename T>
 bool threadpool<T>::append_p(T *request)
 {
@@ -90,6 +85,7 @@ bool threadpool<T>::append_p(T *request)
     m_queuestat.post();
     return true;
 }
+
 template <typename T>
 void *threadpool<T>::worker(void *arg)
 {
@@ -97,6 +93,7 @@ void *threadpool<T>::worker(void *arg)
     pool->run();
     return pool;
 }
+
 template <typename T>
 void threadpool<T>::run()
 {
@@ -153,4 +150,5 @@ void threadpool<T>::run()
         }
     }
 }
+
 #endif
